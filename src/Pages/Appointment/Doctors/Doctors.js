553c5a7d51600b7a11b9e380da13fccf4a156163
immanual19 <immanuelsarker1@gmail.com>
2022-12-14 17:48:42 +0600

    ratingSystemAdded

--- src/Pages/Appointment/Doctors/Doctors.js
+++ src/Pages/Appointment/Doctors/Doctors.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useState } from 'react';
 import avatar1 from '../../../assets/images/avatar1.jpeg';
 const Doctors = ({doctor, setBooking}) => {
   console.log(doctor);
+  let sum=0;
+  doctor.ratingInfo.map(singleRating=>{
+    
+    sum=sum+singleRating.rating;
+    
+  });
     const {schedules}=doctor;
     return (
         <div>
@@ -16,6 +23,7 @@ const Doctors = ({doctor, setBooking}) => {
           <p>Speciality: {doctor.speciality}</p>
           <p>Workplace: {doctor.workplace}</p>
           <p>Fees: {doctor.fees}</p>
+          <p>Rating: {sum/doctor.ratingInfo.length}/5</p>
           <div class="card-actions">
             <label for="booking-modal"disabled={schedules.length===0} 
             onClick={()=>setBooking(doctor)}
